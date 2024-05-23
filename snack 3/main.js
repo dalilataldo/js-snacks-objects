8d4db937f34c2ba586a0262349d8bb2e02e8ab0e
dalilataldo <dalila.taldo@gmail.com>
2024-05-23 10:03:16 +0200

    add: objects

---
+++ snack 3/main.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const animals = [
+    {
+        nome: 'leone',
+        famiglia: 'felidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'cane',
+        famiglia: 'canidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'gallina',
+        famiglia: 'fasianidi',
+        classe: 'uccelli'
+    },
+    {
+        nome: 'serpente',
+        famiglia: 'colubridi',
+        classe: 'rettili'
+    },
+    {
+        nome: 'pecora',
+        famiglia: 'ovini',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'balena',
+        famiglia: 'cetacei',
+        classe: 'uccelli'
+    },
+]
